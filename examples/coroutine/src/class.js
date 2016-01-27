cb07b5db4d10b374ec61d4d3b26af0e8c486f2c6
Trey Wood <edwinjwood3rd@gmail.com>
2016-01-27 17:44:51 -0500

    detect 'await' expressions and transform into coroutine based on that instead of explicit generator syntax

--- examples/coroutine/src/class.js
+++ examples/coroutine/src/class.js
@@ -2,11 +2,11 @@ import Promise from 'bluebird'
 
 export default class MyClass {
 
-  * async runningTotal(numbers) {
+  async runningTotal(numbers) {
 
       let total = 0;
       for (let i = 0; i < numbers.length; i++) {
-        let num = yield Promise.resolve(numbers[i]).delay(1000);
+        let num = await Promise.resolve(numbers[i]).delay(1000);
         console.log(`${total} + ${num} = ${total + num}`);
         total += num;
       }
--- src/index.js
+++ src/index.js
@@ -2,22 +2,43 @@ export default function({ types: t }) {
 
   const REPLACED = Symbol();
   const bluebird = t.identifier("Promise");
+  var funcNode = null;
 
   return {
     visitor: {
-      FunctionExpression(path) {
+
+      AwaitExpression(path) {
         let { node } = path;
+        let arg = node.argument;
+        path.replaceWith(t.yieldExpression(arg));
 
-        // work with async methods
-        if (!node[REPLACED] && node.async === true) {
+        // if there's an 'await', we'll turn the function into a generator
+        if (funcNode) {
+          funcNode.generator = true;
+        }
+      },
 
-          // generators use 'coroutine' helper, other wise just 'method'
-          let methodName = node.generator ? "coroutine": "method";
-          let asyncWrap = t.memberExpression(bluebird, t.identifier(methodName));
+      FunctionExpression: {
 
-          node[REPLACED] = true;
-          node.async = false; // prevent recursive visits
-          path.replaceWith(t.callExpression(asyncWrap, [node]));
+        enter(path) {
+          let { node } = path;
+          if (!node[REPLACED] && node.async === true) {
+            funcNode = node;
+          }
+        },
+
+        exit(path) {
+          if (funcNode && !funcNode[REPLACED] && funcNode.async === true) {
+            // generators use 'coroutine' helper, other wise just 'method'
+            let methodName = funcNode.generator ? "coroutine": "method";
+            let asyncWrap = t.memberExpression(bluebird, t.identifier(methodName));
+
+            funcNode[REPLACED] = true;
+            funcNode.async = false; // prevent recursive visits
+            path.replaceWith(t.callExpression(asyncWrap, [funcNode]));
+
+            funcNode = null;
+          }
         }
       }
     }
