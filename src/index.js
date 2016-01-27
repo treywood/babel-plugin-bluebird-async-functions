7aafa313042ed734562aa2b3361cdf3bb500025a
Trey Wood <edwinjwood3rd@gmail.com>
2016-01-27 01:22:02 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+export default function({ types: t }) {
+
+  const REPLACED = Symbol();
+  const bluebird = t.identifier("Bluebird");
+
+  return {
+    visitor: {
+      FunctionExpression(path) {
+        let { node } = path;
+
+        // work with async methods
+        if (!node[REPLACED] && node.async === true) {
+
+          // generators use 'coroutine' helper, other wise just 'method'
+          let methodName = node.generator ? "coroutine": "method";
+          let asyncWrap = t.memberExpression(bluebird, t.identifier(methodName));
+
+          node[REPLACED] = true;
+          node.async = false; // prevent recursive visits
+          path.replaceWith(t.callExpression(asyncWrap, [node]));
+        }
+      }
+    }
+  };
+}
